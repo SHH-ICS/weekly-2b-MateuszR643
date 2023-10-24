e389044f62cf01963b541d1095290bde40f09465
MateuszR643 <144241927+MateuszR643@users.noreply.github.com>
2023-10-24 01:12:03 +0000

    Wrote assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,15 @@
+import math
 
+r = input("What is the radius?")
+
+if r.isnumeric() ==  False:
+    print("This is not a positive number.")
+elif float(r) < 0:
+    print("This is not a positive number.")
+else:
+    r = float(r)
+    a = math.pi * math.sqrt(r)
+    c = 2 * math.pi * r
+    print("The radius was: ", r, "units/n")
+    print("The area is: ", a, "square units/n")
+    print("The circumference is: ", c, "units/n")
